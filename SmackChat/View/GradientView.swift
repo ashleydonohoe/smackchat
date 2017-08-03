a8b21e779ad35e15462a4cb22d859d1e512c1dba
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-08-02 21:04:44 -0400

    styled channel vc

---
+++ SmackChat/View/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  SmackChat
+//
+//  Created by Ashley Donohoe on 8/2/17.
+//  Copyright © 2017 Ashley Donohoe. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.3631127477, green: 0.4045833051, blue: 0.8775706887, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var BottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, BottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
